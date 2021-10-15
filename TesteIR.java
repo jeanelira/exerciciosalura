7f5ebe7dba9478a1c74fdc17fd534e7a5d021bcf
Jeane Lira Dias <86386701+jeanelira@users.noreply.github.com>
2021-10-15 18:43:55 -0300

    Update TesteIR.java

--- TesteIR.java
+++ TesteIR.java
@@ -2,12 +2,13 @@
 public class TesteIR {
 
 	public static void main(String[] args) {
-	    	// De 1900.0 até 2800.0 o IR é de 7.5% e pode deduzir R$ 142
-	        // De 2800.01 até 3751.0 o IR é de 15% e pode deduzir R$ 350
-	        // De 3751.01 até 4664.00 o IR é de 22.5% e pode deduzir R$ 636
+	    	// De 1900.0 atÃ© 2800.0 o IR Ã© de 7.5% e pode deduzir R$ 142
+	        // De 2800.01 atÃ© 3751.0 o IR Ã© de 15% e pode deduzir R$ 350
+	        // De 3751.01 atÃ© 4664.00 o IR Ã© de 22.5% e pode deduzir R$ 636
 	        double salario = 1980.0;
 	        double impostoSobreSalario = 0;
 	        
+		// VerificaÃ§Ãµes
 	        if (salario >= 1900.0 && salario <= 2800.0 ) {
 	        	impostoSobreSalario = salario * 0.075;
 	        }
